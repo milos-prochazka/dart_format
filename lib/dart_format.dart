57e529c88158e38ad537c38724ad98c7f2be05f5
Miloš Procházka <milos.proch@gmail.com>
2021-06-19 10:12:05 +0200

    Verze 1.0.0

--- lib/dart_format.dart
+++ lib/dart_format.dart
@@ -5,6 +5,7 @@ import 'package:tuple/tuple.dart';
 
 void command(List<String> args)
 {
+  print ('dart-format v1.0.0');
   if (args.isEmpty)
   {
     print('Syntax: dart_format <file or directory> [size of tabulator]');
